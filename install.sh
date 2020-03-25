c76035270db0976f93ef8ade2e93cb064890b11f
ZhuQI <qi.zhu@rakwireless.com>
2020-03-25 17:42:45 +0800

    add repo of chirpstack

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ fi
 
 cp init_sql.sql /tmp/init_sql.sql -f
 
-apt list --upgradable
+#apt list --upgradable
 
 # 1. install requirements
-apt -f -y install dialog mosquitto mosquitto-clients redis-server redis-tools postgresql
+apt -f -y install dialog mosquitto mosquitto-clients redis-server redis-tools postgresql apt-transport-https dirmngr
 
 # 2. setup PostgreSQL databases and users
 sudo -u postgres psql -c "create role chirpstack_as with login password 'dbpassword';"
@@ -25,23 +25,23 @@ rm -f /tmp/init_sql.sql
 #3. install ChirpStack packages
 #3.1 install https requirements
 #apt -f -y install apt-transport-https dirmngr
-#apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1CE2AFD36DBCCA00
-#sudo echo "deb https://artifacts.chirpstack.io/packages/3.x/deb stable main" | sudo tee /etc/apt/sources.list.d/chirpstack.list
-#apt update
-#apt install chirpstack-network-server
-#apt install chirpstack-gateway-bridge
-#apt install chirpstack-application-server
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1CE2AFD36DBCCA00
+echo "deb https://artifacts.chirpstack.io/packages/3.x/deb stable main" | sudo tee /etc/apt/sources.list.d/chirpstack.list
+apt update
+apt install chirpstack-network-server
+apt install chirpstack-gateway-bridge
+apt install chirpstack-application-server
 
-rm *.deb -f
+#rm *.deb -f
 #3.2 download chirpstack packages
-wget https://artifacts.chirpstack.io/downloads/chirpstack-application-server/chirpstack-application-server_3.6.1_linux_amd64.deb
-wget https://artifacts.chirpstack.io/downloads/chirpstack-gateway-bridge/chirpstack-gateway-bridge_3.5.0_linux_amd64.deb
-wget https://artifacts.chirpstack.io/downloads/chirpstack-network-server/chirpstack-network-server_3.6.0_linux_amd64.deb
+#wget https://artifacts.chirpstack.io/packages/3.x/deb/pool/main/c/chirpstack-application-server/chirpstack-application-server_3.5.1_linux_armv7.deb
+#wget https://artifacts.chirpstack.io/packages/3.x/deb/pool/main/c/chirpstack-gateway-bridge/chirpstack-gateway-bridge_3.4.1_linux_armv7.deb
+#wget https://artifacts.chirpstack.io/packages/3.x/deb/pool/main/c/chirpstack-network-server/chirpstack-network-server_3.6.0_linux_armv7.deb
 
 #3.3 install chirpstack packages
-dpkg -i chirpstack-application-server_3.6.1_linux_amd64.deb
-dpkg -i chirpstack-gateway-bridge_3.5.0_linux_amd64.deb
-dpkg -i chirpstack-network-server_3.6.0_linux_amd64.deb
+#dpkg -i chirpstack-application-server_3.5.1_linux_armv7.deb
+#dpkg -i chirpstack-gateway-bridge_3.4.1_linux_armv7.deb
+#dpkg -i chirpstack-network-server_3.6.0_linux_armv7.deb
 
 #4. configure lora
 # configure chirpstack Server
